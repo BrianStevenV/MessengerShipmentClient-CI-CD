4f324935b16c1eef9e13eb9f457d1235b3f142d1
Brian Steven <sbrianprog@gmail.com>
2023-04-27 14:35:09 -0500

    trying

--- src/main/java/com/integrador/client/Controller/ClientController.java
+++ src/main/java/com/integrador/client/Controller/ClientController.java
@@ -73,6 +73,4 @@ public class ClientController {
         return this.clientService.getCustomer(dni);
     }
 
-
-
 }
--- src/test/java/com/integrador/client/ClientApplicationTests.java
+++ src/test/java/com/integrador/client/ClientApplicationTests.java
@@ -1,13 +1,13 @@
-package com.integrador.client;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ClientApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.integrador.client;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ClientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
